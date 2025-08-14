96df3a42d553b6be70c606bcdb89bf884184edb3
Matt Weiden <538456+mweiden@users.noreply.github.com>
2025-08-14 09:48:26 -0700

    Add storage backend options and docs

--- src/storage/mod.rs
+++ src/storage/mod.rs
@@ -6,6 +6,17 @@ pub trait Storage: Send + Sync {
     async fn get(&self, path: &str) -> Result<Vec<u8>, StorageError>;
 }
 
+#[async_trait]
+impl Storage for Box<dyn Storage> {
+    async fn put(&self, path: &str, data: Vec<u8>) -> Result<(), StorageError> {
+        (**self).put(path, data).await
+    }
+
+    async fn get(&self, path: &str) -> Result<Vec<u8>, StorageError> {
+        (**self).get(path).await
+    }
+}
+
 #[derive(thiserror::Error, Debug)]
 pub enum StorageError {
     #[error("io: {0}")]
