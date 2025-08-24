c906f3bb072d97b8df02f4dfa1c2266240b39f48
Matt Weiden <538456+mweiden@users.noreply.github.com>
2025-08-24 15:18:27 -0700

    Vendor protoc for build

--- build.rs
+++ build.rs
@@ -1,4 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let protoc = protoc_bin_vendored::protoc_bin_path()?;
+    unsafe { std::env::set_var("PROTOC", protoc) };
     tonic_build::compile_protos("proto/cass.proto")?;
     Ok(())
 }
